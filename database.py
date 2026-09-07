02e2a5c4db99e1ded5ed75cd508c581c56484fde
Kshitij Gabhane <kshitijgabhane7@gmail.com>
2026-09-08 02:27:56 +0530

    Connect ORCA to cloud MySQL database

--- database.py
+++ database.py
@@ -1,11 +1,15 @@
+import os
 import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def get_db_connection():
-
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="prince@183",
-        database="orca_sos"
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT", "14195")),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
